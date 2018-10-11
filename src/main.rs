fc8545048f91847f51bcf45121564b1744fd3581
Seiya Nuta <nuta@seiya.me>
2018-10-11 12:57:31 +0900

    support executing a file

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,16 @@ extern crate nom;
 extern crate pretty_env_logger;
 extern crate rustyline;
 extern crate nix;
-use rustyline::error::ReadlineError;
+extern crate clap;
 use std::process;
+use std::fs::File;
+use std::io::prelude::*;
+use rustyline::error::ReadlineError;
+use clap::{Arg, App};
 mod exec;
 mod parser;
 
-pub fn read_line_from_stdin() -> String {
+fn read_line_from_stdin() -> String {
     let mut rl = rustyline::Editor::<()>::new();
     match rl.readline("nsh$ ") {
         Ok(line) => {
@@ -22,6 +26,21 @@ pub fn read_line_from_stdin() -> String {
         Err(ReadlineError::Eof) => process::exit(0),
         Err(err) => {
             panic!("something went wrong with readline: {:?}", err);
+        }
+    }
+}
+
+fn exec_file(script_file: &str) {
+    let mut f = File::open(script_file).expect("failed to open a file");
+    let mut script = String::new();
+    f.read_to_string(&mut script).expect("failed to load a file");
+
+    match parser::parse_line(script.as_str()) {
+        Ok(cmd) => {
+            exec::exec(cmd);
+        },
+        Err(err) => {
+            eprintln!("nsh: parse error: {:?}", err);
         }
     }
 }
@@ -41,8 +60,22 @@ fn mainloop() {
 
 fn main() {
     pretty_env_logger::init();
+    let args = App::new("nsh")
+        .version("0.0.0")
+        .author("Seiya Nuta <nuta@seiya.me>")
+        .about("A command-line shell that focuses on performance and productivity.")
+        .arg(Arg::with_name("script")
+            .help("The shell script file.")
+            .required(false)
+            .index(1))
+        .get_matches();
 
-    loop {
-        mainloop();
+    if let Some(script) = args.value_of("script") {
+        exec_file(script);
+    } else {
+        // Interactive mode.
+        loop {
+            mainloop();
+        }
     }
 }
