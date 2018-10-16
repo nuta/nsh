79a19cc24640e88812bb7a413e0030b496d3b2b3
Seiya Nuta <nuta@seiya.me>
2018-10-16 11:05:46 +0900

    reimplement pipeline using fork/exec directly

--- src/exec.rs
+++ src/exec.rs
@@ -1,20 +1,20 @@
-use nix::unistd::pipe;
+use nix::unistd::{pipe, fork, ForkResult, execv, dup2, close, Pid};
+use nix::sys::wait::{waitpid, WaitStatus};
 use parser::{self, Ast, ExpansionOp, RunIf, Span, Word};
 use std::collections::HashMap;
-use std::io;
-use std::os::unix::io::FromRawFd;
-use std::process::{self, ExitStatus, Stdio};
+use std::os::unix::io::RawFd;
+use std::ffi::CString;
 use std::sync::{Arc, Mutex};
 
 #[derive(Debug)]
-pub enum VariableValue {
+pub enum Value {
     String(String),
     Function(Box<parser::Command>),
 }
 
 #[derive(Debug)]
 pub struct Variable {
-    value: VariableValue,
+    value: Value,
 }
 
 #[derive(Debug)]
@@ -27,6 +27,8 @@ pub struct Scope {
 pub struct ExecEnv {
     scope: Scope,
 }
+
+pub type ExitStatus = i32;
 
 lazy_static! {
     static ref CONTEXT: Mutex<ExecEnv> = Mutex::new(ExecEnv {
@@ -48,7 +50,7 @@ pub fn get_var<'a, 'b>(scope: &'a Scope, key: &'b str) -> Option<Arc<Variable>> 
 fn expand_param(scope: &mut Scope, name: &String, op: &ExpansionOp) -> String {
     if let Some(var) = get_var(&scope, name.as_str()) {
         let string_value = match var.value {
-            VariableValue::String(ref s) => s.clone(),
+            Value::String(ref s) => s.clone(),
             _ => panic!("TODO: cannot expand"),
         };
 
@@ -70,7 +72,7 @@ fn expand_param(scope: &mut Scope, name: &String, op: &ExpansionOp) -> String {
                 scope,
                 name,
                 Variable {
-                    value: VariableValue::String(value.clone()),
+                    value: Value::String(value.clone()),
                 },
             );
             value
@@ -107,31 +109,50 @@ fn evaluate_words(scope: &mut Scope, words: &Vec<Word>) -> Vec<String> {
     evaluated
 }
 
-fn exec_command(
-    argv: Vec<String>,
-    stdin: Stdio,
-    stdout: Stdio,
-    stderr: Stdio,
-) -> io::Result<ExitStatus> {
-    process::Command::new(argv[0].clone())
-        .args(argv.into_iter().skip(1))
-        .stdin(stdin)
-        .stdout(stdout)
-        .stderr(stderr)
-        .status()
+fn move_fd(src: RawFd, dst: RawFd) {
+    if src != dst {
+        dup2(src, dst).expect("failed to dup2");
+        close(src).expect("failed to close");
+    }
 }
 
-pub struct CommandResult {
-    status: i32,
+fn exec_command(
+    argv: Vec<String>,
+    stdin: RawFd,
+    stdout: RawFd,
+    stderr: RawFd,
+) -> Pid {
+    match fork().expect("failed to fork") {
+        ForkResult::Parent { child } => {
+            return child;
+        },
+        ForkResult::Child => {
+            // FIXME: CString::new() internally calls memchr(); it could be non-negligible cost.
+            let argv0 = CString::new(argv[0].clone()).unwrap();
+            let mut args = Vec::new();
+            for arg in argv {
+                args.push(CString::new(arg).unwrap());
+            }
+
+            // Prepare child's stdin/stdout/stderr.
+            move_fd(stdin, 0);
+            move_fd(stdout, 1);
+            move_fd(stderr, 2);
+
+            // TODO: inherit exported variables
+            execv(&argv0, &args).expect("failed to exec");
+            unreachable!();
+        },
+    }
 }
 
 fn run_terms(
     scope: &mut Scope,
     terms: &Vec<parser::Term>,
-    mut stdin: Stdio,
-    mut stdout: Stdio,
-    mut stderr: Stdio,
-) -> CommandResult {
+    stdin: RawFd,
+    stdout: RawFd,
+    stderr: RawFd,
+) -> ExitStatus {
     let mut last_status = 0;
     for term in terms {
         for pipeline in &term.pipelines {
@@ -140,46 +161,99 @@ fn run_terms(
                 (true, RunIf::Success) => (),
                 (false, RunIf::Failure) => (),
                 (_, RunIf::Always) => (),
-                _ => continue,
+                _ => return last_status,
             }
 
-            // Invoke commands in a pipeline.
-            let mut iter = pipeline.commands.iter().peekable();
-            while let Some(command) = iter.next() {
-                let next_stdin = if iter.peek().is_some() {
-                    // There is a next command in the pipeline (e.g. date in
-                    // `date | hexdump`). Create and connect a pipe.
-                    let (pipe_out, pipe_in) = pipe().unwrap();
-                    stdout = unsafe { Stdio::from_raw_fd(pipe_in) };
-                    Some(unsafe { Stdio::from_raw_fd(pipe_out) })
-                } else {
-                    None
+            last_status = run_pipeline(scope, pipeline, stdin, stdout, stderr);
+        }
+    }
+    last_status
+}
+
+fn run_pipeline(
+    scope: &mut Scope,
+    pipeline: &parser::Pipeline,
+    mut stdin: RawFd,
+    mut stdout: RawFd,
+    stderr: RawFd
+) -> ExitStatus {
+
+    // Invoke commands in a pipeline.
+    let mut last_command_result = None;
+    let mut iter = pipeline.commands.iter().peekable();
+    let mut childs = Vec::new();
+    let prev_stdout = stdout;
+    while let Some(command) = iter.next() {
+        let pipes = if iter.peek().is_some() {
+            // There is a next command in the pipeline (e.g. date in
+            // `date | hexdump`). Create and connect a pipe.
+            let (pipe_out, pipe_in) = pipe().unwrap();
+            stdout = pipe_in;
+            Some((pipe_out, pipe_in))
+        } else {
+            None
+        };
+
+        last_command_result = Some(run_command(scope, command, stdin, stdout, stderr));
+        if let CommandResult::External { pid } = last_command_result.unwrap() {
+            childs.push(pid);
+        }
+
+        if let Some((next_stdin, pipe_in)) = pipes {
+            stdout = prev_stdout;
+            stdin = next_stdin;
+            close(pipe_in).expect("failed to close pipe_in");
+        }
+    }
+
+    let mut last_status = 0;
+    match last_command_result {
+        None => {
+            trace!("nothing to execute");
+            last_status = 0;
+        },
+        Some(CommandResult::Internal { exited_with }) => {
+            last_status = exited_with;
+        },
+        Some(CommandResult::External { pid: last_pid }) => {
+            for child in childs {
+                let status = match waitpid(Pid::from_raw(-1), None).expect("faied to waitpid") {
+                    WaitStatus::Exited(pid, status) => {
+                        trace!("nsh: pid={} status={}", pid, status);
+                        status
+                    },
+                    WaitStatus::Signaled(pid, status, _) => {
+                        println!("nsh: pid={} signal={:#?}", pid, status);
+                        -1
+                    },
+                    _ => {
+                        // TODO:
+                        panic!("unexpected waitpid event");
+                    }
                 };
 
-                let r = run_command(scope, command, stdin, stdout, stderr);
-                last_status = r.status;
-
-                stdin = Stdio::inherit();;
-                stderr = Stdio::inherit();
-                stdout = Stdio::inherit();;
-                if let Some(next) = next_stdin {
-                    stdin = next;
+                if child == last_pid {
+                    last_status = status;
                 }
             }
         }
     }
 
-    CommandResult {
-        status: last_status,
-    }
+    last_status
+}
+
+#[derive(Debug, Copy, Clone)]
+enum CommandResult {
+    External { pid: Pid },
+    Internal { exited_with: i32 },
 }
 
 fn run_command(
     scope: &mut Scope,
     command: &parser::Command,
-    stdin: Stdio,
-    stdout: Stdio,
-    stderr: Stdio,
+    stdin: RawFd,
+    stdout: RawFd,
+    stderr: RawFd,
 ) -> CommandResult {
     trace!("run_command: {:?}", command);
     match command {
@@ -188,52 +262,51 @@ fn run_command(
             if argv.len() == 0 {
                 // `argv` is empty. For example bash accepts `> foo.txt`; it creates an empty file
                 // named "foo.txt".
-                return CommandResult { status: 0 };
+                return CommandResult::Internal { exited_with: 0 };
             }
 
             // Functions
             if let Some(var) = get_var(scope, argv.get(0).unwrap()) {
-                if let Variable {
-                    value: VariableValue::Function(body),
-                } = var.as_ref()
-                {
+                if let Variable { value: Value::Function(body) } = var.as_ref() {
                     return run_command(scope, &body, stdin, stdout, stderr);
                 }
             }
 
             // External commands
-            let status = match exec_command(argv, stdin, stdout, stderr) {
-                Ok(status) => status.code().unwrap(),
-                Err(_) => {
-                    println!("nsh: failed to execute");
-                    -1
-                }
-            };
-
-            CommandResult { status }
+            let pid = exec_command(argv, stdin, stdout, stderr);
+            return CommandResult::External { pid };
         }
         parser::Command::Assignment { assignments } => {
             for (name, value) in assignments {
                 let value = Variable {
-                    value: VariableValue::String(evaluate_word(scope, value)),
+                    value: Value::String(evaluate_word(scope, value)),
                 };
 
                 set_var(scope, name.as_str(), value)
             }
-            CommandResult { status: 0 }
+            CommandResult::Internal { exited_with: 0 }
         }
         parser::Command::FunctionDef { name, body } => {
-            set_var(
-                scope,
-                name.as_str(),
-                Variable {
-                    value: VariableValue::Function(body.clone()),
-                },
-            );
-            CommandResult { status: 0 }
+            let value = Variable {
+                value: Value::Function(body.clone()),
+            };
+            set_var(scope, name.as_str(), value);
+            CommandResult::Internal { exited_with: 0 }
+        },
+        parser::Command::If { condition, then_part, .. } => {
+            // TODO: else, elif
+            if run_terms(scope, condition, stdin, stdout, stderr) == 0 {
+                    CommandResult::Internal {
+                        exited_with: run_terms(scope, then_part, stdin, stdout, stderr)
+                    }
+            } else {
+                CommandResult::Internal { exited_with: 0 }
+            }
         },
         parser::Command::Group { terms } => {
-            run_terms(scope, terms, stdin, stdout, stderr)
+            CommandResult::Internal {
+                exited_with: run_terms(scope, terms, stdin, stdout, stderr)
+            }
         },
         _ => panic!("TODO:"),
     }
@@ -242,8 +315,10 @@ fn run_command(
 pub fn exec(ast: Ast) {
     trace!("ast: {:#?}", ast);
     let scope = &mut CONTEXT.lock().unwrap().scope;
-    let stdin = Stdio::inherit();
-    let stdout = Stdio::inherit();
-    let stderr = Stdio::inherit();
+
+    // Inherit shell's stdin/stdout/stderr.
+    let stdin = 0;
+    let stdout = 1;
+    let stderr = 2;
     run_terms(scope, &ast.terms, stdin, stdout, stderr);
 }
