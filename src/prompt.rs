245993f8c6b7a4ddf7c4f81646d418c0367b4580
Seiya Nuta <nuta@seiya.me>
2018-10-17 11:36:41 +0900

    prompt: support colors

--- src/prompt.rs
+++ src/prompt.rs
@@ -6,8 +6,22 @@ pub struct Prompt {
 }
 
 #[derive(Debug, PartialEq, Eq)]
+pub enum Color {
+    Red,
+    Blue,
+    Yellow,
+    Green,
+    Cyan,
+    Magenta,
+    Bold,
+    Underline,
+    Reset,
+}
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum Span {
     Literal(String),
+    Color(Color),
     Username,
     Hostname,
     CurrentDir,
@@ -20,6 +34,15 @@ named!(span<Input, Span>,
         | map!(tag!("\\h"), |_| Span::Hostname)
         | map!(tag!("\\W"), |_| Span::CurrentDir)
         | map!(tag!("\\n"), |_| Span::Newline)
+        | map!(tag!("\\c{reset}"), |_| Span::Color(Color::Reset))
+        | map!(tag!("\\c{bold}"), |_| Span::Color(Color::Bold))
+        | map!(tag!("\\c{underline}"), |_| Span::Color(Color::Underline))
+        | map!(tag!("\\c{red}"), |_| Span::Color(Color::Red))
+        | map!(tag!("\\c{blue}"), |_| Span::Color(Color::Blue))
+        | map!(tag!("\\c{green}"), |_| Span::Color(Color::Green))
+        | map!(tag!("\\c{yellow}"), |_| Span::Color(Color::Yellow))
+        | map!(tag!("\\c{cyan}"), |_| Span::Color(Color::Cyan))
+        | map!(tag!("\\c{magenta}"), |_| Span::Color(Color::Magenta))
         | map!(take_while!(|c| c != '\\'), |s| Span::Literal(s.to_string()))
     )
 );
@@ -44,6 +67,17 @@ pub fn draw_prompt(prompt: &Prompt, buf: &mut String) {
     for span in &prompt.spans {
         match span {
             Span::Literal(s) => buf.push_str(&s),
+            Span::Color(c) => match c {
+                Color::Red => buf.push_str("\x1b[31m"),
+                Color::Blue => buf.push_str("\x1b[34m"),
+                Color::Yellow => buf.push_str("\x1b[33m"),
+                Color::Green => buf.push_str("\x1b[32m"),
+                Color::Cyan => buf.push_str("\x1b[36m"),
+                Color::Magenta => buf.push_str("\x1b[35m"),
+                Color::Bold => buf.push_str("\x1b[1m"),
+                Color::Underline => buf.push_str("\x1b[4m"),
+                Color::Reset => buf.push_str("\x1b[0m"),
+            },
             Span::Newline => buf.push_str("\n"),
             // TODO:
             Span::Username => buf.push_str("spam"),
