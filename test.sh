57c84cd3aae6b0431c2829b911c1aedfb8aabe39
Seiya Nuta <nuta@seiya.me>
2018-10-22 10:14:30 +0900

    add test.sh

---
+++ test.sh
@@ -0,0 +1,27 @@
+# A blackbox tests.
+
+func1() {
+    if [ $foo -eq 123456 ]; then
+        echo "error: #1"
+        exit 1
+    fi
+
+    if [ $foo -ne 123 ]; then
+        echo "error: #2"
+        exit 1
+    fi
+
+    foo="this is a string"
+}
+
+foo=${bar:=123}
+func1
+
+if [ "$foo" != "this is a string" ]; then
+    echo "error: #3"
+    exit 1
+fi
+
+
+echo "ok!"
+exit 123
