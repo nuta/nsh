b892879765c1662f9dbf657aaa91416674cbcbb9
Seiya Nuta <nuta@seiya.me>
2018-11-27 08:24:17 +0900

    update a test

--- test/for.sh
+++ test/for.sh
@@ -1,9 +1,17 @@
-for x in foo bar a b c d e
+# Break from a nested for & if.
+for x in loop1----- loop2----- loop3------
 do
-    if [ $x = a ]
-    then
-        break
-    fi
-
     echo $x
+    for y in foo bar a b c d e
+    do
+        if [ $y = a ]
+        then
+            if [ 1 -eq 1 ]
+            then
+                break
+            fi
+        fi
+
+        echo $y
+    done
 done
