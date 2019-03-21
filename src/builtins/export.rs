287bb5d6102bf4c105525ee36a5f4d178a389c75
Seiya Nuta <nuta@seiya.me>
2019-03-21 16:43:29 +0900

    fix export command

--- src/builtins/export.rs
+++ src/builtins/export.rs
@@ -1,10 +1,18 @@
 use crate::builtins::InternalCommandContext;
 use crate::exec::ExitStatus;
+use crate::variable::Value;
 use std::io::Write;
 
 pub fn command(ctx: &mut InternalCommandContext) -> ExitStatus {
-    if let Some(name) = ctx.argv.get(1) {
+    if let Some(arg) = ctx.argv.get(1) {
+        let frags: Vec<&str> = arg.splitn(2, '=').collect();
+        let mut iter = frags.iter();
+        let name = iter.next().unwrap();
         ctx.isolate.export(&name);
+
+        if let Some(value) = iter.next() {
+            ctx.isolate.set(&name, Value::String(value.to_owned().to_string()), false);
+        }
     } else {
         for name in ctx.isolate.exported_names() {
             if let Some(var) = ctx.isolate.get(name) {
