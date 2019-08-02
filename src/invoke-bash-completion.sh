aa9000b591ffdfd471fa0995fbb9bd268872e6c0
Seiya Nuta <nuta@seiya.me>
2019-08-02 21:34:51 +0900

    bash-completion: try using an already existing completion before looking for it

--- src/invoke-bash-completion.sh
+++ src/invoke-bash-completion.sh
@@ -8,18 +8,21 @@ if [[ -r /usr/local/opt/bash-completion@2/share/bash-completion/bash_completion 
     . /usr/local/opt/bash-completion@2/share/bash-completion/bash_completion
 fi
 
-# Load and look for the completion function.
 OLD_IFS="$IFS"
 IFS="#"
 COMP_WORDS=($COMP_WORDS)
 IFS="$OLD_IFS"
 
 CMD="${COMP_WORDS[0]}"
-_completion_loader $CMD
+# Maybe the completion is already defined.
 func="$(complete -p $CMD | awk '{ print $(NF-1) }')"
-
 if [[ -z "$func" ]]; then
-    exit 1
+    # Try bash-completion.
+    _completion_loader $CMD
+    func="$(complete -p $CMD | awk '{ print $(NF-1) }')"
+    if [[ -z "$func" ]]; then
+        exit 1
+    fi
 fi
 
 $func "${COMP_WORDS[@]}"
