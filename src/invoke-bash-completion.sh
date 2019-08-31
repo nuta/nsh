f3838b60329643bbada242d027d184b91b61a112
Seiya Nuta <nuta@seiya.me>
2019-08-31 21:19:46 +0900

    fix invoke-bash-completion.sh

--- src/invoke-bash-completion.sh
+++ src/invoke-bash-completion.sh
@@ -1,3 +1,11 @@
+# Convert $COMP_WORDS into an array.
+OLD_IFS="$IFS"
+IFS="#"
+COMP_WORDS=($COMP_WORDS)
+IFS="$OLD_IFS"
+
+CMD="${COMP_WORDS[0]}"
+
 # Ubuntu
 if [[ -r  /etc/profile.d/bash_completion.sh ]]; then
     .  /etc/profile.d/bash_completion.sh
@@ -5,17 +13,22 @@ fi
 
 # macOS (Homebrew)
 if [[ -r /usr/local/opt/bash-completion@2/share/bash-completion/bash_completion ]]; then
+    # Load helper functions for completions.
+    BASH_COMPLETION_COMPAT_DIR=/invalid-path-not-to-preload-any-completions
     . /usr/local/opt/bash-completion@2/share/bash-completion/bash_completion
-    # FIXME:
-    . /usr/local/etc/bash_completion.d/git-completion.bash
+
+    case $CMD in
+    git) . /usr/local/etc/bash_completion.d/git-completion.bash ;;
+    *)
+        # Search bash-completion for a completion.
+        COMP_DIR=/usr/local/opt/bash-completion@2/share/bash-completion/bash_completion
+        if [[ -f $COMP_DIR/$CMD ]]; then
+            . $COMP_DIR/$CMD
+        fi
+        ;;
+    esac
 fi
 
-OLD_IFS="$IFS"
-IFS="#"
-COMP_WORDS=($COMP_WORDS)
-IFS="$OLD_IFS"
-
-CMD="${COMP_WORDS[0]}"
 # Maybe the completion is already defined.
 func="$(complete -p $CMD | awk '{ print $(NF-1) }')"
 if [[ -z "$func" ]]; then
