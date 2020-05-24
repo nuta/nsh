f98020e68f4a79a0a6c99c252c003581a996bf26
Seiya Nuta <nuta@seiya.me>
2020-05-24 15:46:23 +0900

    export: Accept multiple assignments

--- src/builtins/export.rs
+++ src/builtins/export.rs
@@ -4,20 +4,29 @@ use crate::variable::Value;
 use std::io::Write;
 
 pub fn command(ctx: &mut InternalCommandContext) -> ExitStatus {
-    if let Some(arg) = ctx.argv.get(1) {
-        let frags: Vec<&str> = arg.splitn(2, '=').collect();
-        let mut iter = frags.iter();
-        let name = iter.next().unwrap();
-        ctx.shell.export(&name);
-
-        if let Some(value) = iter.next() {
-            ctx.shell
-                .set(&name, Value::String(value.to_owned().to_string()), false);
-        }
-    } else {
+    if ctx.argv.is_empty() {
         for name in ctx.shell.exported_names() {
             if let Some(var) = ctx.shell.get(name) {
                 writeln!(ctx.stdout, "{}={}", name, var.as_str()).ok();
+            }
+        }
+
+        return ExitStatus::ExitedWith(0);
+    }
+
+    for arg in ctx.argv {
+        let frags: Vec<&str> = arg.splitn(2, '=').collect();
+        let mut iter = frags.iter();
+        match (iter.next(), iter.next()) {
+            (Some(name), Some(value)) => {
+                ctx.shell.export(name);
+                ctx.shell
+                    .set(&name, Value::String(value.to_owned().to_string()), false);
+            }
+            (Some(name), None) => {
+                ctx.shell.export(name);
+            }
+            _ => {
             }
         }
     }
