967842c70e2e7b4afa15fdda71cfc255c4f47d42
Ian Wahbe <ian@wahbe.com>
2021-10-24 16:45:35 -0700

    Add support for `-` in cd

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -6,31 +6,42 @@ use std::path::Path;
 pub fn command(ctx: &mut InternalCommandContext) -> ExitStatus {
     trace!("cd: argv={:?}", ctx.argv);
     let old_dir = std::env::current_dir().expect("failed to getcwd()");
-    let dir = match ctx.argv.get(1) {
-        Some(dir) => {
-            if dir.starts_with('/') {
-                // absolute path
-                dir.clone()
+    let (dir, pushd) = match ctx.argv.get(1).map(|s| s.as_str()) {
+        Some("-") => {
+            if let Some(d) = ctx.shell.popd() {
+                (d, false)
             } else {
+                return ExitStatus::ExitedWith(1);
+            }
+        }
+        Some(dir) if dir.starts_with('/') => (dir.to_string(), true),
+        Some(dir) => {
+            (
                 // relative path
                 Path::new(&old_dir)
-                    .join(dir.clone())
+                    .join(dir.to_string())
                     .to_string_lossy()
-                    .into_owned()
-            }
+                    .into_owned(),
+                true,
+            )
         }
         None => {
             // called with no arguments; defaults to the home directory
-            if let Some(home_dir) = dirs::home_dir() {
-                home_dir.to_string_lossy().into_owned()
-            } else {
-                String::from("/")
-            }
+            (
+                if let Some(home_dir) = dirs::home_dir() {
+                    home_dir.to_string_lossy().into_owned()
+                } else {
+                    String::from("/")
+                },
+                true,
+            )
         }
     };
 
-    // TODO: make this configurable
-    ctx.shell.pushd(old_dir.to_str().unwrap().to_owned());
+    if pushd {
+        // TODO: make this configurable
+        ctx.shell.pushd(old_dir.to_str().unwrap().to_owned());
+    }
 
     match std::env::set_current_dir(&dir) {
         Ok(_) => ExitStatus::ExitedWith(0),
